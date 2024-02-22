02d3e2955339eb4696aad463f0135160b61fd42b
CodeSandbox <support@codesandbox.io>
2024-02-22 09:42:33 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,60 @@ import { useState } from "react";
 import "./App.css";
 
 const phrases = [
-  "No",
-  "Are you sure?",
-  "Really sure?",
-  "Please",
-  "But I really love you",
-  "But I really really love you",
-  "You're breaking my heart :(",
+  "ไม่",
+  "ไม่อยากรู้หรอ?",
+  "เอ๊ะ!!แน่ใจหรอ?",
+  "เปิดดูหน่อยนะ",
+  "ได้โปรด",
+  "ยังไม่กดเปิดดูอีก!!!",
+  "เค้าจะทุบเธอแล้วนะ เปิดดูได้แล้ว :<",
 ];
 
 function App() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
-  const yesButtonSize = (noCount * 20) + 16;
+  const yesButtonSize = noCount * 20 + 16;
 
   function getNoButtonText() {
     return phrases[Math.min(noCount, phrases.length - 1)];
   }
 
   return (
-    <div className="val-container"> {
-      yesPressed ? (
+    <div className="val-container">
+      {" "}
+      {yesPressed ? (
         <>
-          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="yes-pic" />
-          <div className="val-text">Yayyy</div>
+          <img
+            src="https://media.tenor.com/q_zo_A3taR0AAAAi/tkthao219-bubududu.gif"
+            alt="yes-pic"
+          />
+          <div className="val-text">
+            วันนี้เหนื่อยไหมคะ เค้าเอากำลังใจมาฝาก สู้ๆนะคนเก่งของเค้า
+            กอดๆจุ้บๆน๊าาา🤍
+          </div>
         </>
       ) : (
         <>
-          <img src="https://media.tenor.com/vknNeIAWurIAAAAi/tkthao219-bear.gif" alt="bear-asking" />
-          <div className="val-text">Do you love me?</div>
+          <img src="https://media.tenor.com/amRuuQRN6d0AAAAi/love.gif" />
+          <div className="val-text">เค้ามีไรจะบอก</div>
           <div>
-              <button className="button yes-button" style={{ fontSize: yesButtonSize }}
-                onClick={() => setYesPressed(true)}>
-                Yes
-              </button>
-              <button onClick={() => setNoCount(noCount + 1)} className="button no-button">
-                {getNoButtonText()}
-              </button>
+            <button
+              className="button yes-button"
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              เปิด
+            </button>
+            <button
+              onClick={() => setNoCount(noCount + 1)}
+              className="button no-button"
+            >
+              {getNoButtonText()}
+            </button>
           </div>
         </>
       )}
-      
     </div>
   );
-};
+}
 export default App;
